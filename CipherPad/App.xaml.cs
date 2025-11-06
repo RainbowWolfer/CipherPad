0cf7197b460b3568aa889b5923a4251a82fcca4c
RainbowWolfer <rainbowwolfer@outlook.com>
2025-11-06 21:07:52 +0800

    Update HandyControl Style usage

--- CipherPad/App.xaml.cs
+++ CipherPad/App.xaml.cs
@@ -1,16 +1,30 @@
-﻿using RW.Base.WPF;
+﻿using HandyControl.Tools;
+using RW.Base.WPF;
 using RW.Base.WPF.Extensions;
 using System.Diagnostics;
 using System.Globalization;
+using System.Net;
 using System.Windows;
 
 namespace CipherPad;
 
 public partial class App : ApplicationBase {
+	private static App? instance;
+	public static App Instance => instance!;
+
 	public override bool IsRelease => AppConfig.IsRelease;
 
 	public App() {
+		instance = this;
+	}
 
+	protected override void BeforeLoadingModules() {
+		base.BeforeLoadingModules();
+
+		ConfigHelper.Instance.SetWindowDefaultStyle();
+		ConfigHelper.Instance.SetNavigationWindowDefaultStyle();
+
+		ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 	}
 
 	public override Window GetMainWindow() => new MainWindow();
--- CipherPad/MainWindow.xaml.cs
+++ CipherPad/MainWindow.xaml.cs
@@ -1,9 +1,30 @@
-﻿using System.Windows;
+﻿using HandyControl.Themes;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace CipherPad;
 
-public partial class MainWindow : Window {
+public partial class MainWindow : HandyControl.Controls.Window {
 	public MainWindow() {
 		InitializeComponent();
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e) {
+		ThemeManager.Current.ApplicationTheme = ApplicationTheme.Dark;
+	}
+
+	private void Button_Click_1(object sender, RoutedEventArgs e) {
+		ThemeManager.Current.ApplicationTheme = ApplicationTheme.Light;
+	}
+
+	private void Button_Click_2(object sender, RoutedEventArgs e) {
+	}
+
+	private void Button_Click_3(object sender, RoutedEventArgs e) {
+		new MainWindow().Show();
+	}
+
+	private void Button_Click_4(object sender, RoutedEventArgs e) {
+		Panel.Children.Add(new Button() { Content = "??" });
 	}
 }
